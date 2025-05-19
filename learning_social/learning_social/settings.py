01f91f990a424061c8231039637b5b3eea87a0cd
Emmanuel Guefif <eguefif@fastmail.com>
2025-05-18 20:21:59 -0400

    LS1: impl model

--- learning_social/learning_social/settings.py
+++ learning_social/learning_social/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "user.apps.UserConfig",
+    "learning_space.apps.LearningSpaceConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
