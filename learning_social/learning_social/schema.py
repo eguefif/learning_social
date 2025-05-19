c1cb664f6756d823def9810b7d2876ace932cb5f
Emmanuel Guefif <eguefif@fastmail.com>
2025-05-18 20:51:27 -0400

    LS1-add field for learning space by user id

--- learning_social/learning_social/schema.py
+++ learning_social/learning_social/schema.py
@@ -18,11 +18,13 @@ class UserType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    learning_spaces_by_user = graphene.List(LearningSpaceType)
+    learning_spaces_by_user_id = graphene.List(
+        LearningSpaceType, user_id=graphene.String(required=True)
+    )
     user_by_id = graphene.Field(UserType, id=graphene.String(required=True))
 
-    def resolve_learning_spaces(root, info):
-        return LearningSpace.objects.all()
+    def resolve_learning_spaces_by_user_id(root, info, user_id):
+        return LearningSpace.objects.filter(user=user_id)
 
     def resolve_user_by_id(root, info, id):
         try:
